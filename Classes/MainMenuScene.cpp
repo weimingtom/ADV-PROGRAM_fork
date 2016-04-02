b9880d5867fd356ccb7999f14a122e8a3d97bb04
SunnyRx <sunny558@qq.com>
2016-04-02 14:06:10 +0800

    改变为Last Day的主菜单界面

--- Classes/MainMenuScene.cpp
+++ Classes/MainMenuScene.cpp
@@ -41,38 +41,32 @@ bool MainMenu::init()
 	Vec2 origin = Director::getInstance()->getVisibleOrigin();
 	//设定背景层
 	auto backgroundLayer = LayerColor::create(Color4B::BLACK);
-	auto backgroundEffect1 = Sprite::create("ui/backgroundEffect/fullscreen_smoke.png");
-	auto backgroundEffect2 = Sprite::create("ui/backgroundEffect/fullscreen_smoke_2.png");
-	backgroundEffect1->setPosition(Vec2(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y));
-	backgroundEffect2->setPosition(Vec2(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y));
+	auto backgroundjpg = Sprite::create("ui/Title/50mRoadRain.jpg");
+	backgroundjpg->setPosition(Vec2(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y));
+	backgroundLayer->addChild(backgroundjpg, 0);
+
 	//logo
-	auto gameTitle = Sprite::create("ui/game_title.png");
-	gameTitle->setPosition(Vec2(433, 358));
+
 
 	//菜单按钮
-	auto buttonNew = MenuItemImage::create("ui/Title/button_new.png", "ui/Title/button_new_down.png", CC_CALLBACK_0(MainMenu::newgame, this));
-	buttonNew->setPosition(Vec2(900, visibleSize.height - 243));
+	auto buttonNew = MenuItemImage::create("ui/Title/mainNewNormal.png", "ui/Title/mainNewTouch.png", CC_CALLBACK_0(MainMenu::newgame, this));
+	buttonNew->setPosition(Vec2(900, visibleSize.height - 250));
 
-	auto buttonLoad = MenuItemImage::create("ui/Title/button_load.png", "ui/Title/button_load_down.png", CC_CALLBACK_0(MainMenu::load, this));
-	buttonLoad->setPosition(Vec2(900, visibleSize.height - 293));
+	auto buttonLoad = MenuItemImage::create("ui/Title/mainLoadNormal.png", "ui/Title/mainLoadTouch.png", CC_CALLBACK_0(MainMenu::load, this));
+	buttonLoad->setPosition(Vec2(900, visibleSize.height - 300));
 
-	auto buttonCG = MenuItemImage::create("ui/Title/button_cg.png", "ui/Title/button_cg_down.png");
-	buttonCG->setPosition(Vec2(900, visibleSize.height - 343));
+	auto buttonConfig = MenuItemImage::create("ui/Title/mainConfigNormal.png", "ui/Title/mainConfigTouch.png",CC_CALLBACK_0(MainMenu::config, this));
+	buttonConfig->setPosition(Vec2(900, visibleSize.height - 350));
 
-	auto buttonConfig = MenuItemImage::create("ui/Title/button_config.png", "ui/Title/button_config_down.png",CC_CALLBACK_0(MainMenu::config, this));
-	buttonConfig->setPosition(Vec2(900, visibleSize.height - 393));
+	//auto buttonExit = MenuItemImage::create("ui/Title/button_exit.png", "ui/Title/button_exit_down.png",CC_CALLBACK_1(MainMenu::menuExit, this));
+	//buttonExit->setPosition(Vec2(900, visibleSize.height - 443));
 
-	auto buttonExit = MenuItemImage::create("ui/Title/button_exit.png", "ui/Title/button_exit_down.png",CC_CALLBACK_1(MainMenu::menuExit, this));
-	buttonExit->setPosition(Vec2(900, visibleSize.height - 443));
-
-	auto menu = Menu::create(buttonNew, buttonLoad, buttonCG, buttonConfig, buttonExit, NULL);
+	auto menu = Menu::create(buttonNew, buttonLoad, buttonConfig, NULL);
 	menu->setPosition(Vec2::ZERO);
 
 
 
-	backgroundLayer->addChild(backgroundEffect1, 0);
-	backgroundLayer->addChild(backgroundEffect2, 1);
-	backgroundLayer->addChild(gameTitle, 3);
+	
 
 	backgroundLayer->addChild(menu, 4);
 	this->addChild(backgroundLayer, 0);
